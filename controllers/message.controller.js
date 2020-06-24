55b848b0796eebde36cf4556606640fa55ce277c
Pablo <jvmiramontes@mticonsultoria.com>
2020-06-24 00:21:36 -0500

    feat: Get Sent Messages and Received Messages     + Se ha agregado función que obtiene los mensajes recividos por el usuario autenticado.     + Se ha agregado función que obtiene los mensajes enviados por el usuario autenticado.     + Se ha agregado función que obtiene el conteo de mensajes no leídos.

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -27,6 +27,7 @@ function saveMessage(req, res) {
     message.receiver = req.body.receiver;
     message.text = req.body.text;
     message.created_at = moment().unix();
+    message.viewed = false;
     
     message.save((err, messageStored) => {
         if(err) return res.status(500).send({message: 'Error at request'});
@@ -41,13 +42,13 @@ function saveMessage(req, res) {
         * @param {Response} res Respuesta HTTP
         * @return {JSON}  {total, pages, messages}
         */
-function getReceivedMessage(req, res) {
+function getReceivedMessages(req, res) {
     let page = 1;
     const itemsPerPage = 4;
 
     if(req.params.page) page = req.params.page;
     
-    Message.find({receiver: req.user.sub}).populate('emitter').paginate(page, itemsPerPage, (err, messages, total) => {
+    Message.find({receiver: req.user.sub}).populate('emitter', 'name surname image nick _id').paginate(page, itemsPerPage, (err, messages, total) => {
         if(err) return res.status(500).send({message: 'Error at request'});
         if(!messages) return res.status(404).send({message: 'No messages'});
         return res.status(200).send({
@@ -57,8 +58,47 @@ function getReceivedMessage(req, res) {
         });
     });
 }
+
+        /**FUNCTION getEmittedMessages
+        * Permite obtener los mensajes enviados por el usuario autenticado.
+        * @param {Request} req Petición HTTP
+        * @param {Response} res Respuesta HTTP
+        * @return {JSON}  {total, pages, messages}
+        */
+function getEmittedMessages(req, res) {
+    let page = 1;
+    const itemsPerPage = 4;
+
+    if(req.params.page) page = req.params.page;
+    
+    Message.find({emitter: req.user.sub}).populate('emitter receiver', 'name surname image nick _id').paginate(page, itemsPerPage, (err, messages, total) => {
+        if(err) return res.status(500).send({message: 'Error at request'});
+        if(!messages) return res.status(404).send({message: 'No messages'});
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            messages
+        });
+    });
+}
+
+        /**FUNCTION getUnseenMessages
+        * Permite obtener el conteo de los mensajes no vistos por el usuario autenticado.
+        * @param {Request} req Petición HTTP
+        * @param {Response} res Respuesta HTTP
+        * @return {JSON}  {unviewed}
+        */
+function getUnseenMessages(req, res) {
+    Message.count({receiver: req.user.sub, viewed: false}).exec((err, count) => {
+        if(err) return res.status(500).send({message: 'Error at request'});
+        return res.status(200).send({unviewed: count});
+    });
+}
+
 module.exports = {
     test,
     saveMessage,
-    getReceivedMessage
+    getReceivedMessages,
+    getEmittedMessages,
+    getUnseenMessages
 }
--- routes/message.route.js
+++ routes/message.route.js
@@ -7,7 +7,9 @@ const md_auth = require('./../middlewares/authenticated');
 
 api.get('/test-message', md_auth.ensureAuth, MessageController.test);
 api.post('/message', md_auth.ensureAuth, MessageController.saveMessage);
-api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getReceivedMessage);
+api.get('/messages-received/:page?', md_auth.ensureAuth, MessageController.getReceivedMessages);
+api.get('/messages-sent/:page?', md_auth.ensureAuth, MessageController.getEmittedMessages);
+api.get('/messages-unseen', md_auth.ensureAuth, MessageController.getUnseenMessages);
 
 
 module.exports = api;
